eb8385df4889a8522c86e435c6c3a71d908a8809
Yadukvind <93067144+Yadukvind@users.noreply.github.com>
2021-10-25 14:39:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@efcinema",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🎥Our Channel🎥', url="https://t.me/efcinema"
                                 )
                         ]
                     ]
